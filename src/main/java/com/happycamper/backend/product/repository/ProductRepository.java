4f3eed3d276df8443d32be5a78e28fe196d3fb0b
jeongdawun <jeongdawun33@gmail.com>
2023-07-09 02:22:51 +0900

    fix: [정다운] 상품 등록시 중복된 상품명은 사용 불가 쿼리 누락 부분 추가[SHC-23]

--- src/main/java/com/happycamper/backend/product/repository/ProductRepository.java
+++ src/main/java/com/happycamper/backend/product/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.happycamper.backend.product.repository;
 import com.happycamper.backend.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByProductName(String productName);
 }

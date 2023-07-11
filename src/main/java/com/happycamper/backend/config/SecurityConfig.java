b369d44b852cd6e153919ccfb2c21e9219e60a6e
jeongdawun <jeongdawun33@gmail.com>
2023-07-11 10:17:13 +0900

    chore: [정다운] EncoderConfig, SecurityConfig 분리 [SHC-26]

--- src/main/java/com/happycamper/backend/config/SecurityConfig.java
+++ src/main/java/com/happycamper/backend/config/SecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -20,11 +19,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Value("${jwt.password}")
     private String secretKey;

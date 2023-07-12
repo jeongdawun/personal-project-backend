49b73054cede9fafba545fc0daf9bc5af2649cf3
jeongdawun <jeongdawun33@gmail.com>
2023-07-12 09:22:15 +0900

    chore: [정다운] 인스턴스 서버 CorsConfig에 추가 [SHC-1]

--- src/main/java/com/happycamper/backend/config/CorsConfig.java
+++ src/main/java/com/happycamper/backend/config/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins(
                         "http://127.0.0.1:8080",
                         "http://localhost:8080",
-                        "http://3.38.61.219")
+                        "http://3.38.61.219",
+                        "http://ec2-3-38-61-219.ap-northeast-2.compute.amazonaws.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .exposedHeaders(HttpHeaders.AUTHORIZATION)

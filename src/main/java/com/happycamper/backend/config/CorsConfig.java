0d6fc73b57e6ce78380efdd1d37cbbdf07ae9203
jeongdawun <jeongdawun33@gmail.com>
2023-07-12 21:32:30 +0900

    chore: [정다운] CorsConfig 인스턴스 주소 숨기기 실패로 원상복구 [SHC-1]

--- src/main/java/com/happycamper/backend/config/CorsConfig.java
+++ src/main/java/com/happycamper/backend/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.happycamper.backend.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,22 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("${ec2.server1}")
-    private String ec2url;
-
-    @Value("${ec2.server2}")
-    private String ec2url2;
-
-    @Value("${ec2.server3}")
-    private String ec2url3;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://127.0.0.1:8080",
                         "http://localhost:8080",
-                        ec2url, ec2url2, ec2url3)
+                        "http://3.38.61.219",
+                        "http://ec2-3-38-61-219.ap-northeast-2.compute.amazonaws.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .exposedHeaders(HttpHeaders.AUTHORIZATION)

3117bc5b4aa86b913a93d8faa705d1aa4e016f8e
jeongdawun <jeongdawun33@gmail.com>
2023-07-12 20:34:57 +0900

    chore: [정다운] CorsConfig 인스턴스 주소 숨기기 시도 [SHC-1]

--- src/main/java/com/happycamper/backend/config/CorsConfig.java
+++ src/main/java/com/happycamper/backend/config/CorsConfig.java
@@ -15,13 +15,16 @@ public class CorsConfig implements WebMvcConfigurer {
     @Value("${ec2.server2}")
     private String ec2url2;
 
+    @Value("${ec2.server3}")
+    private String ec2url3;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://127.0.0.1:8080",
                         "http://localhost:8080",
-                        ec2url, ec2url2)
+                        ec2url, ec2url2, ec2url3)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .exposedHeaders(HttpHeaders.AUTHORIZATION)
